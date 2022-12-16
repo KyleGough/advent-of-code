48edd16041732ace7975f7e4de6c6ca8ea37b17f
Kyle Gough <kylegough98@gmail.com>
2022-12-16 00:13:34 +0000

    Stricter types for 2015 Day 12

--- solutions/2015/12/p2.ts
+++ solutions/2015/12/p2.ts
@@ -1,15 +1,19 @@
 import { getPuzzle } from '@utilities/getPuzzle';
 import { run } from '@utilities/run';
 
-export const day12p2 = (input: string) => {
-  return getNumberSum(JSON.parse(input));
-};
+type Document =
+  | Array<DocumentBasis>
+  | {
+      [index: string]: DocumentBasis;
+    };
 
-const getNumberSum = (input: any): number => {
+type DocumentBasis = string | number | Document;
+
+const getNumberSum = (input: Document): number => {
   let sum = 0;
 
   // Input is an array.
-  if (input.length) {
+  if (Array.isArray(input)) {
     for (let i = 0; i < input.length; i++) {
       const value = input[i];
       if (typeof value === 'number') {
@@ -20,24 +24,27 @@ const getNumberSum = (input: any): number => {
         sum += getNumberSum(value);
       }
     }
-    return sum;
-  }
-
-  // Input is an object.
-  for (const item in input) {
-    const value = input[item];
-    if (typeof value === 'number') {
-      sum += value;
-    } else if (value === 'red') {
-      return 0;
-    } else if (typeof value === 'string') {
-      continue;
-    } else {
-      sum += getNumberSum(value);
+  } else {
+    // Input is an object.
+    for (const item in input) {
+      const value = input[item];
+      if (typeof value === 'number') {
+        sum += value;
+      } else if (value === 'red') {
+        return 0;
+      } else if (typeof value === 'string') {
+        continue;
+      } else {
+        sum += getNumberSum(value);
+      }
     }
   }
 
   return sum;
+};
+
+export const day12p2 = (input: string) => {
+  return getNumberSum(JSON.parse(input));
 };
 
 const input = getPuzzle(__dirname).input;
