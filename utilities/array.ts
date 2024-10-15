0980af7c1099100caa767cfb92c724e01c7a9893
Kyle Gough <kylegough98@gmail.com>
2024-10-15 23:12:31 +0100

    Array equality optimisation

--- utilities/array.ts
+++ utilities/array.ts
@@ -1,4 +1,8 @@
 export const isArrayEqual = <T>(arr1: T[], arr2: T[]): boolean => {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
       return false;
